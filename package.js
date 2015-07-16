dced094ad143298bcd84252f0f294a705d405142
Zhenya <zhenya@meteor.com>
2015-07-16 13:44:46 -0700

    added debugOnly

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'zhenya:debug',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'some debugging tools',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/zhenyasav/debug',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  debugOnly: true
 });
 
 Package.onUse(function(api) {
